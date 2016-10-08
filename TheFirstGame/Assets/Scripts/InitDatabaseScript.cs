ea6d20e5763b00fbd6b22cfa7f95dc74efa6f469
方迪 <18810868716@163.com>
2016-10-08 18:48:20 +0800

    1. 创建数据库； 2. 创建Login表； 3. 提供Login表操作接口；

---
+++ TheFirstGame/Assets/Scripts/InitDatabaseScript.cs
@@ -0,0 +1,38 @@
+﻿//********************************************
+// File Name  :         InitDatabaseScript.cs
+// Author     :         Andy Fang
+// Create Time:         9/6/2016 2:52:09 PM
+//********************************************
+using UnityEngine;
+
+public class InitDatabaseScript : MonoBehaviour
+{
+
+    #region Unity Callback Method
+
+    void Start ()
+    {
+        if (!PlayerPrefs.HasKey ("FirstRun"))
+        {
+            InitDataBaseAction ();
+            PlayerPrefs.SetString ("FirstRun", "");
+        }
+    }
+
+    #endregion
+
+
+    #region Private Method
+
+    private void InitDataBaseAction ()
+    {
+        // Create Tables
+
+        // Login Table
+        string sql = string.Format("CREATE TABLE IF NOT EXISTS Login(id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, username TEXT NOT NULL, password TEXT NOT NULL, online BOOL DEFAULT false)");
+        ObtainLoginData.Instance.sDb.ExecSql (sql);
+    }
+
+    #endregion
+
+}
